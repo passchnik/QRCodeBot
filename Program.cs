9ec97d2f2bba4111210dab228c87cd224ca08c41
Borys <passchnik.2012@gmail.com>
2021-02-02 21:48:50 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Telegram.Bot;
+
+
+namespace QRCodeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            TelegramBotClient telegramBot = new TelegramBotClient(Startup.token);
+
+            Console.WriteLine($"{telegramBot.GetMeAsync().Result.Username} started");
+
+            telegramBot.OnMessage += (e, arg) =>
+            {
+
+                if (arg.Message.Text == "/start" )
+                {
+                    string txt = "Hi. Send photo or text and i will turn it into QR code";
+                    telegramBot.SendTextMessageAsync(
+                        chatId: arg.Message.Chat.Id,
+                        text: txt
+                        );//
+                }
+                else
+                {
+                    string textToConvert = "ERROR: QR didn't generated";
+                    textToConvert = arg.Message.Text;
+                    QRCodeGenerator qRCode = new QRCodeGenerator();
+                    QRCodeData data = qRCode.CreateQrCode(textToConvert, QRCodeGenerator.ECCLevel.Q);
+                    QRCode code = new QRCode(data);
+
+                    Bitmap bitmap = new Bitmap(code.GetGraphic(10));
+
+                    /*
+                     * for local check
+                    string path = "D:/111.jpg";
+                    bitmap.Save(path);
+                    */
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bitmap.Save(ms, ImageFormat.Png);
+
+                        ms.Position = 0;
+                        telegramBot.SendPhotoAsync(
+                        chatId: arg.Message.Chat.Id,
+                        ms
+                        );
+                    }
+   
+                }
+            };
+
+            telegramBot.StartReceiving();
+
+            Console.ReadLine();
+
+        }
+    }
+}
